d119223e3a0c6902966bc693357132f9dab1063d
kareemeltahan <kareem.eltahan70@gmail.com>
2024-12-06 23:15:56 +0200

    latest ui changes

--- app/ImageEditorScreen.jsx
+++ app/ImageEditorScreen.jsx
@@ -12,7 +12,7 @@ import * as MediaLibrary from "expo-media-library";
 import { cropImage, applyFilter, rotateImage } from "./imageUtils";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRoute } from "@react-navigation/native";
-import { imageToMatrix } from "./imageProcessing";
+// import { imageToMatrix } from "./imageProcessing";
 import {
   Crop,
   ArrowLeft,
@@ -26,6 +26,11 @@ import {
   Contrast,
   Droplet,
   Filter,
+  BookDashed,
+  Circle,
+  SwitchCamera,
+  Radar,
+  Grip,
 } from "lucide-react-native";
 import { Canvas, Image, useImage } from "@shopify/react-native-skia";
 import { StatusBar } from "expo-status-bar";
@@ -48,6 +53,9 @@ const ImageEditorScreen = ({ navigation }) => {
   const [isBrightnessVisible, setIsBrightnessVisible] = useState();
   const [isContrastVisible, setIsContrastVisible] = useState();
   const [isSaturationVisible, setIsSaturationVisible] = useState();
+  const [selectedIntensityTool, setSelectedIntensityTool] = useState(null);
+  const [thresholdingVisible, setThresholdingVisible] = useState(false);
+  const [selectedBlurTool, setSelectedBlurTool] = useState(null);
   const editedImage = useImage(editedImageUri);
 
   const filters = [
@@ -56,6 +64,21 @@ const ImageEditorScreen = ({ navigation }) => {
     { id: "cool", name: "Cool" },
     { id: "warm", name: "Warm" },
     { id: "vintage", name: "Vintage" },
+  ];
+
+  const intensityTools = [
+    { id: "log", name: "Log" },
+    { id: "power_law", name: "Power Law" },
+  ];
+
+  const ThresholdingTools = [
+    { id: "simple", name: "Simple" },
+    { id: "segmentation", name: "Segmentation" },
+    { id: "otsu", name: "Otsu" },
+  ];
+  const BlurTools = [
+    { id: "Gaussian", name: "Gaussian" },
+    { id: "Bilateral", name: "Bilateral" },
   ];
 
   const tools = [
@@ -82,6 +105,36 @@ const ImageEditorScreen = ({ navigation }) => {
       name: "Filters",
       icon: Filter,
       action: () => handleToolSelect("filters"),
+    },
+    {
+      id: "otsuThresholding",
+      name: "Otsu",
+      icon: BookDashed,
+      action: () => handleToolSelect("otsuThresholding"),
+    },
+    {
+      id: "Intensitytransformation",
+      name: "Intensity",
+      icon: Circle, 
+      action: () => handleToolSelect("Intensitytransformation"),
+    },
+    {
+      id: "Histogram equalization",
+      name: "Histogram",
+      icon: SwitchCamera,
+      action: () => handleToolSelect("Histogram equalization"),
+    },
+    {
+      id: "Thresholding",
+      name: "Thresholding",
+      icon: Radar,
+      action: () => handleToolSelect("Thresholding"),
+    },
+    {
+      id: "Blur",
+      name: "Blur",
+      icon: Grip,
+      action: () => handleToolSelect("Blur"),
     },
     {
       id: "rotate",
@@ -141,12 +194,17 @@ const ImageEditorScreen = ({ navigation }) => {
       setIsContrastVisible(false);
       setIsSaturationVisible(false);
       setShowFilters(false);
+      setSelectedIntensityTool(null);
+      setThresholdingVisible(false);
+      
     } else {
       setSelectedTool(toolId);
       setIsBrightnessVisible(toolId === "brightness");
       setIsContrastVisible(toolId === "contrast");
       setIsSaturationVisible(toolId === "saturation");
       setShowFilters(toolId === "filters");
+      setSelectedIntensityTool(toolId === "Intensitytransformation");
+      setThresholdingVisible(toolId === "Thresholding");
     }
   };
 
@@ -216,75 +274,175 @@ const ImageEditorScreen = ({ navigation }) => {
             </Text>
           </View>
         )}
-        {isContrastVisible && (
-        <View className="px-5 pb-4 pt-2 border-slate-300 border-b-2">
-          <Slider
-            style={{ width: "100%", height: 40 }}
-            minimumValue={1}
-            maximumValue={10}
-            step={1}
-            value={contrastValue}
-            onValueChange={(value) => setContrastValue(value)}
-            onSlidingComplete={() => handleAdjustment("contrast")}
-            minimumTrackTintColor="#FFFFFF"
-            maximumTrackTintColor="#000000"
-            thumbTintColor="#0000FF"
-          />
-          <Text className="text-white text-center mt-2">
-            Contrast: {contrastValue}
-          </Text>
-        </View>
-      )}
 
-      {isSaturationVisible && (
-        <View className="px-5 pb-4 pt-2 border-slate-300 border-b-2">
-          <Slider
-            style={{ width: "100%", height: 40 }}
-            minimumValue={1}
-            maximumValue={10}
-            step={1}
-            value={saturationValue}
-            onValueChange={(value) => setSaturationValue(value)}
-            onSlidingComplete={() => handleAdjustment("saturation")}
-            minimumTrackTintColor="#FFFFFF"
-            maximumTrackTintColor="#000000"
-            thumbTintColor="#0000FF"
-          />
-          <Text className="text-white text-center mt-2">
-            Saturation: {saturationValue}
-          </Text>
-        </View>
-      )}
-      {showFilters && (
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          style={{ marginTop: 10, paddingHorizontal: 5 }}
-          className="flex-grow-0 border-slate-300 p-3 border-b-2"
-        >
-          {filters.map((filter) => (
-            <TouchableOpacity
-              key={filter.id}
-              onPress={() => handleFilterApply(filter.id)}
-              className={`flex items-center justify-center w-24 py-5 mx-1 rounded-md ${
-                selectedTool === filter.id ? "bg-primary" : "bg-neutral-700"
-              }`}
-              style={{
-                borderWidth: 1,
-                borderColor: selectedTool === filter.id ? "#1E90FF" : "#333",
-              }}
+        {isContrastVisible && (
+          <View className="px-5 pb-4 pt-2 border-slate-300 border-b-2">
+            <Slider
+              style={{ width: "100%", height: 40 }}
+              minimumValue={1}
+              maximumValue={10}
+              step={1}
+              value={contrastValue}
+              onValueChange={(value) => setContrastValue(value)}
+              onSlidingComplete={() => handleAdjustment("contrast")}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+              thumbTintColor="#0000FF"
+            />
+            <Text className="text-white text-center mt-2">
+              Contrast: {contrastValue}
+            </Text>
+          </View>
+        )}
+
+        {isSaturationVisible && (
+          <View className="px-5 pb-4 pt-2 border-slate-300 border-b-2">
+            <Slider
+              style={{ width: "100%", height: 40 }}
+              minimumValue={1}
+              maximumValue={10}
+              step={1}
+              value={saturationValue}
+              onValueChange={(value) => setSaturationValue(value)}
+              onSlidingComplete={() => handleAdjustment("saturation")}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+              thumbTintColor="#0000FF"
+            />
+            <Text className="text-white text-center mt-2">
+              Saturation: {saturationValue}
+            </Text>
+          </View>
+        )}
+
+        {selectedTool === "Intensitytransformation" && (
+          <View className="px-5 pb-4 pt-2 border-slate-300 border-b-2">
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              className="flex-grow-0"
             >
-              <Text
-                className={`text-xs text-center ${
-                  selectedTool === filter.id ? "text-white" : "text-gray-200"
+              {intensityTools.map((tool) => (
+                <TouchableOpacity
+                  key={tool.id}
+                  onPress={() => setSelectedIntensityTool(tool.id)}
+                  className={`flex items-center justify-center w-28 py-5 mx-6 rounded-md ${
+                    selectedIntensityTool === tool.id ? "bg-primary" : "bg-neutral-700"
+                  }`}
+                  style={{
+                    borderWidth: 1,
+                  
+                  }}
+                >
+                  <Text
+                    className={`text-xs text-center ${
+                      selectedIntensityTool === tool.id ? "text-white" : "text-gray-200"
+                    }`}
+                  >
+                    {tool.name}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
+        )}
+  {thresholdingVisible && (
+  <View className="px-5 pb-4 pt-2 border-slate-300 border-b-2">
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="flex-grow-0"
+    >
+      {ThresholdingTools.map((tool) => (
+        <TouchableOpacity
+          key={tool.id}
+          onPress={() => {
+            setSelectedIntensityTool(tool.id);
+            console.log(`${tool.name} selected`);
+          }}
+          className={`flex items-center justify-center w-20 py-5 mx-3 rounded-md ${
+            selectedIntensityTool === tool.id ? "bg-primary" : "bg-neutral-700"
+          }`}
+          style={{
+            borderWidth: 1,
+            borderColor: selectedIntensityTool === tool.id ? "#1E90FF" : "#333",
+          }}
+        >
+          <Text
+            className={`text-xs text-center ${
+              selectedIntensityTool === tool.id ? "text-white" : "text-gray-200"
+            }`}
+          >
+            {tool.name}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  </View>
+)}
+{selectedTool === "Blur" && (
+  <View className="px-5 pb-4 pt-2 border-slate-300 border-b-2">
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="flex-grow-0"
+    >
+      {BlurTools.map((tool) => (
+        <TouchableOpacity
+          key={tool.id}
+          onPress={() => setSelectedBlurTool(tool.id)} // تحديد الأداة المختارة
+          className={`flex items-center justify-center w-28 py-5 mx-6 rounded-md ${
+            selectedBlurTool === tool.id ? "bg-primary" : "bg-neutral-700"
+          }`}
+          style={{
+            borderWidth: 1,
+            borderColor: selectedBlurTool === tool.id ? "#1E90FF" : "#333",
+          }}
+        >
+          <Text
+            className={`text-xs text-center ${
+              selectedBlurTool === tool.id ? "text-white" : "text-gray-200"
+            }`}
+          >
+            {tool.name}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  </View>
+)}
+
+
+        {showFilters && (
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={{ marginTop: 10, paddingHorizontal: 5 }}
+            className="flex-grow-0 border-slate-300 p-3 border-b-2"
+          >
+            {filters.map((filter) => (
+              <TouchableOpacity
+                key={filter.id}
+                onPress={() => handleFilterApply(filter.id)}
+                className={`flex items-center justify-center w-24 py-5 mx-1 rounded-md ${
+                  selectedTool === filter.id ? "bg-primary" : "bg-neutral-700"
                 }`}
+                style={{
+                  borderWidth: 1,
+                  borderColor: selectedTool === filter.id ? "#1E90FF" : "#333",
+                }}
               >
-                {filter.name}
-              </Text>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      )}
+                <Text
+                  className={`text-xs text-center ${
+                    selectedTool === filter.id ? "text-white" : "text-gray-200"
+                  }`}
+                >
+                  {filter.name}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        )}
 
         {/* Tools Bar Section */}
         <View className="pt-2 pb-4 bg-neutral-900 rounded-t-xl">
@@ -309,11 +467,7 @@ const ImageEditorScreen = ({ navigation }) => {
             ))}
           </ScrollView>
         </View>
-        
       </View>
-
-
-      
     </SafeAreaView>
   );
 };
